006dffba516204b1e5090a28b592dad2bbbb587b
Mustafa <MustafaGino1@yahoo.com>
2022-12-10 21:44:42 -0600

    Created a function called addtoCollections and tested it.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,29 @@
 console.log('***** Music Collection *****')
+
+// created a empty array called Collections
+let collection = [];
+// function named addtoCollection.
+function addtoCollection(title, artist, yearPublished)
+{ const musicobj = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,};
+
+collection.push(musicobj);
+return musicobj;
+}
+
+//Testing addToCollection Function.
+console.log(addtoCollection('ye', 'Kanye West', '2018'));
+console.log(addtoCollection("Goodbye & Good Riddance", 'Juice Wrld', '2018'));
+console.log(addtoCollection('17', 'xxxtentacion', '2017'));
+console.log(addtoCollection('Stoney', 'PostMalone', '2016'));
+console.log(addtoCollection('Black Chrome Hearts', 'Manza', '2020'));
+console.log(addtoCollection('American Idiot', 'Green Day', '2004'));
+
+console.log(collection);
+
+
+
+
+
